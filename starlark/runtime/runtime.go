72c20c96096a064b8ca8321f7339332ec10da523
Máximo Cuadros <mcuadros@gmail.com>
2019-07-10 01:53:49 +0200

    starlark/runtime: basic modules load
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

--- starlark/runtime/runtime.go
+++ starlark/runtime/runtime.go
@@ -1,13 +1,15 @@
 package runtime
 
 import (
-	"os"
-
+	"github.com/ascode-dev/ascode/starlark/module/filepath"
+	"github.com/ascode-dev/ascode/starlark/module/os"
 	"github.com/ascode-dev/ascode/starlark/types"
 	"github.com/ascode-dev/ascode/terraform"
 	"github.com/qri-io/starlib/encoding/base64"
 	"github.com/qri-io/starlib/encoding/csv"
 	"github.com/qri-io/starlib/encoding/json"
+	"github.com/qri-io/starlib/http"
+	"github.com/qri-io/starlib/re"
 	"go.starlark.net/repl"
 	"go.starlark.net/resolve"
 	"go.starlark.net/starlark"
@@ -29,9 +31,14 @@ func NewRuntime(pm *terraform.PluginManager) *Runtime {
 	return &Runtime{
 		fallbackLoad: repl.MakeLoad(),
 		modules: map[string]LoadModuleFunc{
+			filepath.ModuleName: filepath.LoadModule,
+			os.ModuleName:       os.LoadModule,
+
 			"encoding/json":   json.LoadModule,
 			"encoding/base64": base64.LoadModule,
 			"encoding/csv":    csv.LoadModule,
+			"re":              re.LoadModule,
+			"http":            http.LoadModule,
 		},
 		predeclared: starlark.StringDict{
 			"provider": types.BuiltinProvider(pm),
@@ -42,7 +49,7 @@ func NewRuntime(pm *terraform.PluginManager) *Runtime {
 
 func (r *Runtime) ExecFile(filename string) (starlark.StringDict, error) {
 	thread := &starlark.Thread{Name: "thread", Load: r.load}
-	return starlark.ExecFile(thread, os.Args[1], nil, r.predeclared)
+	return starlark.ExecFile(thread, filename, nil, r.predeclared)
 }
 
 func (r *Runtime) load(t *starlark.Thread, module string) (starlark.StringDict, error) {
